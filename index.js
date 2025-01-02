1e616e79cb0bb8bde302603b51cd4305884b7ff1
unknown <nahidujjamanraj@gmail.com>
2025-01-02 15:29:06 +0600

    I bring the data from api

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const bring_data=async()=>{
-    const bring_data=await fetch('')
+    const bring_data=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const con_json=await bring_data.json();
+    console.log(con_json.data)
 }
+bring_data()
