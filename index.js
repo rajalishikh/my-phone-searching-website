037b28d9efec4034a5d8990b65b0f47ec1724063
unknown <nahidujjamanraj@gmail.com>
2025-01-02 15:46:25 +0600

    I show phone data using api

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const bring_data=async()=>{
     const bring_data=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const con_json=await bring_data.json();
-    console.log(con_json.data)
+    show_data(con_json.data)
 }
+// show the data 
+function show_data(bring){
+    const bring_container=document.getElementById('container_all_data')
+    for(let data of bring){
+        console.log(data)
+        const structure=document.createElement('div')
+        structure.innerHTML=`
+        <div class="card bg-base-100 w-96 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img
+      src=${data.image}
+      alt="Shoes"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        
+        `
+        
+        bring_container.appendChild(structure)
+        
+    }
+
+}
+
 bring_data()
