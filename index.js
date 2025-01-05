b662326d2b9fa99b4b8427c523e07ed90815d3b9
unknown <nahidujjamanraj@gmail.com>
2025-01-05 19:08:32 +0600

    I wright a code that can add loader

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const bring_data=async(search_name)=>{
 // show the data 
 function show_data(bring){
   console.log(bring.length)
-  const bring_data=document.getElementById('show_all')
+  const bring_data=document.getElementById('show_all');
+  // set the condition when total data is more then 12 add hidden and remobe hidden
   if(bring.length > 12){
     bring_data.classList.remove('hidden')
   }
   else{
     bring_data.classList.add('hidden')
   }
+  // limit the data 
   bring=bring.slice(0,12)
     const bring_container=document.getElementById('container_all_data');
+    // refresh the  data section 
     bring_container.textContent='';
+    // show the data 
     for(let data of bring){
         console.log(data)
         const structure=document.createElement('div')
@@ -37,18 +41,34 @@ function show_data(bring){
     </div>
   </div>
 </div>
-        
-        `
+`
         
         bring_container.appendChild(structure)
         
     }
+    add_loader(false)
+    
 
 }
-// search your phone 
 
+// search your phone 
 function search_your_phone(){
+   add_loader(true)
+
     const find_search=document.getElementById('input')
     const man=find_search.value;
     bring_data(man)
 }
+
+const add_loader= (is_loading) => {
+  const loader_part=document.getElementById('loader_part')
+  if(is_loading){
+    loader_part.classList.remove('hidden')
+  }
+  else{
+    loader_part.classList.add('hidden')
+
+  }
+
+}
+
